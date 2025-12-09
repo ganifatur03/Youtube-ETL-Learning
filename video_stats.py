6d82741a1b33cb567af74717375d5483070f119e
Gani Faturahman <ghanifatturahman@gmail.com>
2025-12-09 14:25:21 +0700

    Add function to extract video datas

--- video_stats.py
+++ video_stats.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 YT_API = os.getenv("YOUTUBE_API")
-channel_handle = "officialhigedandism"
+channel_handle = "PewDiePie"
 maxResult = 50
 video_id_limit = 60
 
@@ -59,7 +59,49 @@ def get_video_ids(playlist_id):
 
   except requests.exceptions.RequestException as e:
     raise e
-  
+
+def extract_video_data(video_ids):
+  extracted_data = []
+
+  def batch_list(video_id_list, batch_size):
+    """Yield successive n-sized chunks from video_id_list."""
+    for i in range(0, len(video_id_list), batch_size):
+        yield video_id_list[i:i + batch_size]
+
+  try:
+      for batch in batch_list(video_ids, maxResult):
+        video_id_str = ",".join(batch)
+        
+        url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_id_str}&key={YT_API}"
+
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses
+
+        data = response.json()
+
+        for item in data.get("items", []):
+           video_id = item['id']
+           snippet = item['snippet']
+           contentDetails = item['contentDetails']
+           statistics = item['statistics']
+           
+           video_data = {
+              "video_id": video_id,
+              "title": snippet.get("title"),
+              "publishedAt": snippet.get("publishedAt"),
+              "duration": contentDetails.get("duration"),
+              "viewCount": statistics.get("viewCount", None),
+              "likeCount": statistics.get("likeCount", None),
+              "commentCount": statistics.get("commentCount", None),
+           }
+
+           extracted_data.append(video_data)
+
+      return extracted_data
+   
+  except requests.exceptions.RequestException as e:
+     raise e
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
     print(f"Playlist ID: {playlist_id}")
